da41981edb90e6d0032ff264f3b38071b345fc97
Sander Star <1424bcbe-40d8-4137-822a-a19074aba054>
2022-02-11 12:21:32 +0000

    Add karma

---
+++ karma.conf.js
@@ -0,0 +1,23 @@
+module.exports = function(config) {
+    config.set({
+  
+      frameworks: ["ui5", "qunit", "sinon"],
+  
+      ui5: {
+        url: "https://sapui5.hana.ondemand.com",
+        mode: "script",
+        config: {
+          async: true,
+          resourceRoots: {
+            "nl.gasunie.supplier.supplier": "./base/webapp"
+          }
+        },
+        tests: [
+          "nl/gasunie/supplier/supplier/test/unit/AllTests"
+        ]
+      },
+  
+      browsers: ["ChromeHeadless"]
+  
+    });
+  };

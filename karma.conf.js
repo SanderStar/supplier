9f842e7733acffbfcb2d57ef98019be5ee28a0be
Sander Star <1424bcbe-40d8-4137-822a-a19074aba054>
2022-02-11 14:41:17 +0000

    Karma coverage report

--- karma.conf.js
+++ karma.conf.js
@@ -16,9 +16,39 @@ module.exports = function(config) {
           "nl/gasunie/supplier/supplier/test/unit/AllTests"
         ]
       },
-  
+
+      preprocessors: {
+        "{webapp/**/*.js,webapp/!(test|view)}/**/!(mock*).js": ["coverage"]
+      },
+      coverageReporter: {
+              includeAllSources: true,            
+        reporters: [
+          {
+            type: "html",
+            dir: "coverage"
+          },
+          {
+            type: "text"
+          }
+        ],
+        check: {
+          each: {
+            statements: 50,
+            branches: 50,
+            functions: 50,
+            lines: 50
+          }
+        }
+      },
+
+      reporters: ["progress", "coverage"],
+
       browsers: ["ChromiumHeadless"],
-  
+      
+      browserConsoleLogOptions: {
+        level: "error"
+      },
+
       singleRun: false,
       autoWatch: true      
     });
--- webapp/test/unit/controller/Object.controller.js
+++ webapp/test/unit/controller/Object.controller.js
@@ -1,14 +1,30 @@
 /*global QUnit*/
 
 sap.ui.define([
-	"nl/gasunie/supplier/supplier/controller/Object.controller"
-], (Controller) => {
+	"nl/gasunie/supplier/supplier/controller/Object.controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/routing/History"    
+], (Controller, JSONModel, History) => {
 	"use strict";
 
 	QUnit.module("Object Controller");
 
 	QUnit.test("I should test the Object controller", function (assert) {
 		var oAppController = new Controller();
+        const oOwnerComponentStub = {
+            getRouter: () => ({
+                getRoute: () => ({
+                    attachPatternMatched: () => ""
+                })
+            })
+        };
+
+        const oModel =  new JSONModel({
+            busy : true,
+            delay : 0
+        });
+        sinon.stub(oAppController, "getRouter").returns(oOwnerComponentStub.getRouter());
+        sinon.stub(oAppController, "setModel").returns(oModel)        
 		oAppController.onInit();
 		assert.ok(oAppController);
 	});
@@ -17,7 +33,50 @@ sap.ui.define([
 		var oAppController = new Controller();
 		assert.ok(oAppController.onInit);
 	});
+    
+    // onNavBack
+	QUnit.test("I should test the Object controller onNavBack", function (assert) {
+        const oOwnerComponentStub = {
+            getRouter: () => ({
+                navTo: () => ({
+                })
+            })
+        };
+		var oAppController = new Controller();
+        sinon.stub(oAppController, "getRouter").returns(oOwnerComponentStub.getRouter());
+		assert.ok(oAppController.onNavBack());
+	});
 
+    QUnit.test("It should check onNavBack() is available", (assert) => {
+        // Arrange
+        var oAppController = new Controller();          
+        const oSpy = sinon.spy(oAppController, "onNavBack");
+
+        // Act
+        oAppController.onNavBack();
+
+        // Assert
+        assert.ok(oSpy.calledOnce);
+    });
+
+    QUnit.test("It should check onNavBack() calling the getRouter.navTo()", (assert) => {
+        // Arrange
+        var oAppController = new Controller();          
+        sinon.stub(oAppController, "getRouter").callsFake(() => ({
+            navTo: () => ""
+        }));
+        sinon.stub(History, "getInstance").callsFake(() => ({
+            getPreviousHash: () => undefined
+        }));
+        const oSpy = sinon.spy(oAppController, "onNavBack");
+
+        // Act
+        oAppController.onNavBack("fakeRoute");
+
+        // Assert
+        assert.ok(oSpy.calledOnce);
+    });
+    
 	QUnit.test("I should test the View1 controller has getAufnr", (assert) => {
 		var oAppController = new Controller();
 		sinon.stub(oAppController, "getAufnr").returns('1000216');
